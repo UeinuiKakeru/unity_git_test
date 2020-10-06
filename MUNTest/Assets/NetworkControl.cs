a32abd2511e070fb6379eb8121db54606840d3fe
UeinuiKakeru <k.ueinui@gmail.com>
2020-10-06 14:04:50 +0900

    no message

---
+++ MUNTest/Assets/NetworkControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+
+public class NetworkControl : MonobitEngine.MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // OnGUI is called for rendering and handring GUI events
+    void OnGUI()
+    {
+        
+    }
+}
